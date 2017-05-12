99955f6dd0280003e7ae075a518b10dfaa10efb7
Mauve3 <mauvemoonman@gmail.com>
2017-05-12 16:47:29 -0700

    test

--- conn_nic2.py
+++ conn_nic2.py
@@ -11,3 +11,27 @@ sock = ssl.wrap_socket(sock, keyfile="occasio.key", certfile="cacert.pem",
                        ca_certs="/home/bitnami/occasio/python2713/lib/python2.7/site-packages/certifi/cacert.pem")
 
 sock.connect(('ote.nic.io', 700))
+
+login_com = """ <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+                  <command>
+                    <login>
+                      <clID>NIC-1253</clID>
+                      <pw>.[&lt;2&amp;q'xKn9NMdD:</pw>
+                    <options>
+                      <version>1.0</version>
+                      <lang>en</lang>
+                    </options>
+                    <svcs>
+                      <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+                      <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+                      <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
+                    </svcs>
+                    </login>
+                       <clTRID>1xl2gXUrXDbb</clTRID>
+                  </command>
+               </epp>
+            """
+
+sock.send(login_com)
+print sock.recv(1280)
+sock.close()
