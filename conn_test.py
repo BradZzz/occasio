b71ec6fe19b0b134b3f851bf0d87c77c7ecadf9d
Mauve3 <mauvemoonman@gmail.com>
2017-05-12 15:17:01 -0700

    test

---
+++ conn_test.py
@@ -0,0 +1,9 @@
+import pycurl
+
+curl = pycurl.Curl()
+curl.setopt(pycurl.CAINFO, "root.pem")
+curl.setopt(pycurl.SSL_VERIFYPEER, 1)
+curl.setopt(pycurl.SSL_VERIFYHOST, 2)
+curl.setopt(pycurl.URL, "https://google.com/")
+
+curl.perform()
