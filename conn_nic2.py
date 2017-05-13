84d9dd8ce72adc6c385cd78da16a526e06b963c3
Mauve3 <mauvemoonman@gmail.com>
2017-05-12 17:18:39 -0700

    test

--- conn_nic2.py
+++ conn_nic2.py
@@ -3,10 +3,10 @@ import socket, ssl
 #client = EppClient(ssl_keyfile='occasio.key', ssl_certfile='cacert.pem', ssl_cacerts='root.pem')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(60)  # regular timeout
+sock.settimeout(120)  # regular timeout
 
 sock = ssl.wrap_socket(sock, keyfile="occasio.key", certfile="cacert.pem",
-                       server_side=False,
+                       server_side=True,
                        cert_reqs=ssl.CERT_REQUIRED,
                        ca_certs="/home/bitnami/occasio/python2713/lib/python2.7/site-packages/certifi/cacert.pem")
 
@@ -25,7 +25,9 @@ def hello():
   print "\n"
 
 def login():
-  login_com = """ <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+  login_com = """
+                 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                 <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
                     <command>
                       <login>
                         <clID>NIC-1253</clID>
@@ -47,8 +49,6 @@ def login():
   sock.send(login_com)
   print sock.recv(1280)
   print "\n"
-
-print sock.recv(1280)
 
 login()
 hello()
