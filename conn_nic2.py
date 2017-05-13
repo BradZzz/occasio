47f51a9660910cc8f4dc919693f51688455482eb
Mauve3 <mauvemoonman@gmail.com>
2017-05-12 17:01:42 -0700

    test

--- conn_nic2.py
+++ conn_nic2.py
@@ -12,22 +12,32 @@ sock = ssl.wrap_socket(sock, keyfile="occasio.key", certfile="cacert.pem",
 
 sock.connect(('ote.nic.io', 700))
 
+hello = """
+          <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+          <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+            <hello/>
+          </epp>
+        """
+
+sock.send(hello)
+print sock.recv(1280)
+
 login_com = """ <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
                   <command>
                     <login>
                       <clID>NIC-1253</clID>
                       <pw>.[&lt;2&amp;q'xKn9NMdD:</pw>
-                    <options>
-                      <version>1.0</version>
-                      <lang>en</lang>
-                    </options>
-                    <svcs>
-                      <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
-                      <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
-                      <objURI>urn:ietf:params:xml:ns:host-1.0</objURI>
-                    </svcs>
+                      <options>
+                        <version>1.0</version>
+                        <lang>en</lang>
+                      </options>
+                      <svcs>
+                        <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+                        <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+                        <objURI>urn:ietf:params:xml:ns:secDNS-1.1</objURI>
+                      </svcs>
                     </login>
-                       <clTRID>1xl2gXUrXDbb</clTRID>
+                    <clTRID>1xl2gXUrXDbb</clTRID>
                   </command>
                </epp>
             """
