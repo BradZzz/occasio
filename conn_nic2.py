a98fcb0b232dd43bb509ce96cd0f54365f5e3725
Mauve3 <mauvemoonman@gmail.com>
2017-05-16 13:53:00 -0700

    fixed shell

--- conn_nic2.py
+++ conn_nic2.py
@@ -6,6 +6,7 @@ parser.add_argument('action')
 parser.add_argument('domain')
 args = parser.parse_args()
 
+print args
 print json.dumps({ 'action' : args.action })
 print json.dumps({ 'domain' : args.domain })
 
