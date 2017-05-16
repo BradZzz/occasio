a3cc77ed7b386d6e9320233aff5d573680339f26
Mauve3 <mauvemoonman@gmail.com>
2017-05-16 13:54:18 -0700

    fixed shell

--- conn_nic2.py
+++ conn_nic2.py
@@ -18,16 +18,16 @@ sock = ssl.wrap_socket(sock, keyfile="occasio.key", certfile="cacert.pem", serve
 try:
   sock.connect(('ote.nic.io', 700))
   print json.dumps({ 'greeting' : sock.recv().decode("latin1") })
-  command = Command(args.domain, args.action, sock)
-  command.login()
-
-  actions = {
-    'create' : command.createDomain,
-    'backorder' : command.backorder,
-  }
-
-  if args.a in actions:
-    actions[args.a]()
+  # command = Command(args.domain, args.action, sock)
+  # command.login()
+  #
+  # actions = {
+  #   'create' : command.createDomain,
+  #   'backorder' : command.backorder,
+  # }
+  #
+  # if args.a in actions:
+  #   actions[args.a]()
 
 finally:
   sock.close()
