0338cc299c3b576969feb0cc97b561c9d71748ba
BradZzz <brad.zimmerman@episource.com>
2017-06-20 10:22:00 -0700

    no mongo

--- models/bids.js
+++ models/bids.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose')
-
-var bidSchema = new mongoose.Schema({
-  usrID:    { type: String, required: true },
-  domID:    { type: String, required: true },
-  amount:   { type: Number, required: true },
-  created:  { type: Date, default: Date.now },
-  __v:      { type: Number, select: false },
-})
-
-module.exports = mongoose.model('bids', bidSchema)
+//var mongoose = require('mongoose')
+//
+//var bidSchema = new mongoose.Schema({
+//  usrID:    { type: String, required: true },
+//  domID:    { type: String, required: true },
+//  amount:   { type: Number, required: true },
+//  created:  { type: Date, default: Date.now },
+//  __v:      { type: Number, select: false },
+//})
+//
+//module.exports = mongoose.model('bids', bidSchema)
--- models/domain.js
+++ models/domain.js
@@ -1,12 +1,12 @@
-var mongoose = require('mongoose')
-
-var domainSchema = new mongoose.Schema({
-  name:     { type: String, required: true },
-  expires:    { type: Date, required: true },
-  value:     { type: Number, default: 0 },
-  meta:   { type: mongoose.Schema.Types.Mixed },
-  created:  { type: Date, default: Date.now },
-  __v:      { type: Number, select: false },
-})
-
-module.exports = mongoose.model('domain', domainSchema)
+//var mongoose = require('mongoose')
+//
+//var domainSchema = new mongoose.Schema({
+//  name:     { type: String, required: true },
+//  expires:    { type: Date, required: true },
+//  value:     { type: Number, default: 0 },
+//  meta:   { type: mongoose.Schema.Types.Mixed },
+//  created:  { type: Date, default: Date.now },
+//  __v:      { type: Number, select: false },
+//})
+//
+//module.exports = mongoose.model('domain', domainSchema)
