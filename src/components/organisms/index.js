e1275cfdcd4732a7db0a22faed45546c4970fa6b
BradZzz <brad.zimmerman@episource.com>
2017-10-20 15:51:29 -0700

    push for save

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -5,4 +5,5 @@ export { default as HeaderPanel } from "./HeaderPanel/HeaderPanel";
 export { default as ListDetailPanel } from "./ListDetailPanel/ListDetailPanel";
 export { default as NavPanel } from "./NavPanel/NavPanel";
 export { default as TitlePanel } from "./TitlePanel/TitlePanel";
+export { default as BookPanel } from "./BookPanel/BookPanel";
 
